2cd89bbfe371153491e1a3e120b8f632a5f1197b
Patrick Burroughs (Celti) <celti@celti.name>
2016-06-10 09:19:05 -0700

    setup: make all config certificate-specific

--- nfsn-setup.sh
+++ nfsn-setup.sh
@@ -8,26 +8,28 @@ echo " + Cloning letsencrypt.sh git repository..."
 git submodule init
 git submodule update
 
-echo " + Generating alias configuration..."
+echo " + Generating configuration..."
 for site_root in $(nfsn list-aliases); do
    if [[ -d "${DOCUMENT_ROOT}${site_root}/" ]]; then
       WELLKNOWN="${DOCUMENT_ROOT}${site_root}/${well_known}"
       CONFIGDIR="letsencrypt.sh/certs/${site_root}/"
       mkdir -p "${WELLKNOWN}" "${CONFIGDIR}"
       echo "WELLKNOWN='${WELLKNOWN}'" > "${CONFIGDIR}/config"
+      echo " + Installing hook script..."
+      echo "HOOK='$(realpath nfsn-hook.sh)'" >> "${CONFIGDIR}/config"
+      chmod +x nfsn-hook.sh
       unset single_cert
    fi
 done
 
-if [[ "$single_cert" ]]; then
+if [[ "${single_cert:+true}" ]]; then
    echo " + Generating fallback configuration..."
    mkdir -p "${DOCUMENT_ROOT}${well_known}"
    echo "WELLKNOWN='${DOCUMENT_ROOT}${well_known}'" > letsencrypt.sh/config
+   echo " + Installing hook script..."
+   echo "HOOK='$(realpath nfsn-hook.sh)'" >> letsencrypt.sh/config
+   chmod +x nfsn-hook.sh
 fi
-
-echo " + Installing hook script..."
-chmod +x nfsn-hook.sh
-echo "HOOK='$(realpath nfsn-hook.sh)'" >> letsencrypt.sh/config
 
 echo " + Generating domains.txt..."
 nfsn ${single_cert:+-s} list-aliases > letsencrypt.sh/domains.txt
