0d3b3e44a24e46a8c024e70f8ebcd5858ebca316
Michal Nazarewicz <mina86@mina86.com>
2016-06-10 21:25:31 +0200

    cron: use grep instead of implementing contains

--- nfsn-cron.sh
+++ nfsn-cron.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
-contains() {
-	local item; local needle="$1"
-	shift && local -a haystack=("$@")
-	for item in "${haystack[@]}"; do
-		[[ $item = $needle ]] && return 0
-	done
-	return 1
-}
-
 cd "$(dirname "$0")"
 
 echo " + Updating lets-nfsn.sh..."
@@ -20,8 +11,9 @@ git submodule update --remote
 cd letsencrypt.sh
 
 echo " + Checking certificate expiration date..."
-declare -a checks=( $(find certs -name cert.pem -type l -exec openssl x509 -checkend 2592000 -in {} \;) )
-if contains "Certificate will expire" "${checks[@]}"; then
+if find certs -name cert.pem -type l \
+	-exec openssl x509 -checkend 2592000 -in {} \; |
+		grep -qF "Certificate will expire"; then
 	echo " + Certificte will expire in 30 days or less! SSH into this site and"
 	echo "   run the following commands to renew your certificates:"
 	echo "	cd ~/lets-nfsn.sh/letsencrypt.sh/"
