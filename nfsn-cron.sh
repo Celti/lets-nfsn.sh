08be4671cd14ee3e628fe13cfcfb5368cbbc82a0
Michal Nazarewicz <mina86@mina86.com>
2016-06-12 15:23:52 +0200

    cron: type fix

--- nfsn-cron.sh
+++ nfsn-cron.sh
@@ -14,7 +14,7 @@ echo " + Checking certificate expiration date..."
 if find certs -name cert.pem -type l \
 	-exec openssl x509 -checkend 2592000 -in {} \; |
 		grep -qF "Certificate will expire"; then
-	echo " + Certificte will expire in 30 days or less! SSH into this site and"
+	echo " + Certificate will expire in 30 days or less! SSH into this site and"
 	echo "   run the following commands to renew your certificates:"
 	printf '	%q --cron\n' "$(realpath letsencrypt.sh)"
 	echo " + This error message will repeat daily."
