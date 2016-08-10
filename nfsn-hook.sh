53b9fc28387b85d33469b6feea602ec25139704d
Michal Nazarewicz <mina86@mina86.com>
2016-08-10 15:28:19 +0200

    hook: silence shellcheck warnings
    The unused variables are present for documentation purposes so it’s
    fine that they are not used.

--- nfsn-hook.sh
+++ nfsn-hook.sh
@@ -2,22 +2,26 @@
 set -o errexit -o nounset -o pipefail
 
 function deploy_challenge {
+	# shellcheck disable=SC2034
 	local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
 	echo " + No hook enabled for deploying challenges."
 }
 
 function clean_challenge {
+	# shellcheck disable=SC2034
 	local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
 	echo " + No hook enabled for cleaning challenges."
 }
 
 function deploy_cert {
+	# shellcheck disable=SC2034
 	local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}" TIMESTAMP="${6}"
 	echo " + Installing new certificate for ${DOMAIN}..."
 	cat "$KEYFILE" "$CERTFILE" "$CHAINFILE" | nfsn -i set-tls
 }
 
 function unchanged_cert {
+	# shellcheck disable=SC2034
 	local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
 	echo " + Certificate for ${DOMAIN} unchanged."
 	# echo " + Resetting dead man's switch..."
